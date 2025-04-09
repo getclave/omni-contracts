f2cec35e0ff1b259c12711bf3bb09f2fd7c031b2
tahos81 <tahirozpala@gmail.com>
2025-04-09 15:26:17 +0300

    chore: delete docgen

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,30 +13,26 @@ const SHOULD_ENABLE_VIA_IR: Boolean = process.env.SHOULD_ENABLE_VIA_IR
   : true;
 
 const config: HardhatUserConfig = {
-	solidity: {
-		version: "0.8.27",
-		settings: {
-			viaIR: SHOULD_ENABLE_VIA_IR,
-			optimizer: {
-				enabled: true,
-				runs: 999,
-				details: {
-					yul: true,
-				},
-			},
-			evmVersion: "cancun",
-		},
-	},
-	networks: {
-		hardhat: {
-			allowUnlimitedContractSize: true,
-		},
-		...dynamicNetworkConfig(),
-	},
-	docgen: {
-		projectName: "Nexus",
-		projectDescription: "Nexus - Biconomy Modular Smart Account - ERC-7579",
-	},
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      viaIR: SHOULD_ENABLE_VIA_IR,
+      optimizer: {
+        enabled: true,
+        runs: 999,
+        details: {
+          yul: true,
+        },
+      },
+      evmVersion: "cancun",
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    ...dynamicNetworkConfig(),
+  },
 };
 
 export default config;
